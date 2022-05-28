07e32cbf430931c166d211b2edbc68c1c0d53916
Eralpozcan <eralpozcans@gmail.com>
2022-05-28 21:35:51 +0300

    init

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-container grid-list-xl>
+    <h2 class="pl-4">
+      <span class="blue--text">Portfolio</span>
+    </h2>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-dialog
+        v-model="project.dialog"
+        lazy
+        max-width="1000"
+        v-for="project in projects"
+        :key="project.title"
+      >
+        <template v-slot:activator="{ on }">
+          <v-flex xs12 sm6 md4 lg4 xl4 v-on="on">
+            <v-card hover flat color="transparent">
+              <v-img
+                :src="project.poster"
+                :alt="project.title"
+                height="230"
+                lazy-src="https://cdn.dribbble.com/users/503653/screenshots/3143656/fluid-loader.gif"
+              ></v-img>
+              <v-card-title primary-title class="justify-center">{{project.title}}</v-card-title>
+            </v-card>
+          </v-flex>
+        </template>
+        <v-card v-if="project.dialog">
+          <v-img :src="project.poster"></v-img>
+          <v-card-actions class="justify-center">
+            <h3 class="headline mb-0">
+              <span>Technology</span>
+            </h3>
+          </v-card-actions>
+          <v-card-actions class="justify-center">
+            <v-chip color="blue" text-color="white">{{project.tech.tech1}}</v-chip>
+            <v-chip color="blue" text-color="white">{{project.tech.tech2}}</v-chip>
+            <v-chip color="blue" text-color="white">{{project.tech.tech3}}</v-chip>
+            <v-chip color="blue" text-color="white">{{project.tech.tech4}}</v-chip>
+          </v-card-actions>
+          <v-card-actions class="justify-center">
+            <v-btn flat large dark color="blue" :href="project.git" target="_blank">
+              <v-icon left>fab fa-github</v-icon>GitHub
+            </v-btn>
+            <v-btn large flat dark color="blue" :href="project.demo" target="_blank">
+              <v-icon left>fas fa-desktop</v-icon>Demo
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Portfolio',
+    titleTemplate: '%s ← Eralp Özcan',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'description',
+        content:
+          'Eralp Özcan Portfolio Vue Vue.js Nuxt Nuxt.js HTML CSS Vuetify Axios  JavaScript Web Developer Front-End Responsive'
+      },
+      { charset: 'utf-8' },
+      { property: 'og:title', content: 'Eralp Özcan' },
+      { property: 'og:site_name', content: 'Eralp Özcan' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://eralpozcan.com' },
+      {
+        property: 'og:image',
+        content: ''
+      },
+      {
+        property: 'og:description',
+        content:
+          'Eralp Özcan Portfolio Vue Vue.js Nuxt Nuxt.js HTML CSS Vuetify Axios  JavaScript Web Developer Front-End Responsive'
+      }
+    ]
+  },
+  data () {
+    return {
+      dialog: false,
+      projects: [
+        {
+          dialog: false,
+          title: 'MoviePal',
+          git: 'https://github.com/Eralpozcan/MoviesApp',
+          demo: 'https://moviepal.vercel.app/',
+          tech: {
+            tech1: 'Vue',
+            tech2: 'JavaScript',
+            tech3: 'Bootstrap',
+            tech4: 'Axios'
+          },
+          poster: 'https://i.hizliresim.com/8r97ekp.png'
+        },
+        {
+          dialog: false,
+          title: 'Energy-Dashboard',
+          git: 'https://github.com/Eralpozcan/Energy-Dashboard',
+          demo: 'https://energy-dashboard.vercel.app',
+          tech: {
+            tech1: 'Vue',
+            tech2: 'Vuetify',
+            tech3: 'Bootstrap',
+            tech4: 'Node.js'
+          },
+          poster: 'https://i.hizliresim.com/o27jxla.png'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,201 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-flex xs12 sm12 md5 lg6 xl6 class="mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Education</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">
+                  Dec 2021
+                </div>
+                <div class="caption font-weight-bold blue--text">Feb 2021</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Full Stack Web Development Bootcamp </strong>
+                <div class="caption mb-2">Patika.Dev</div>
+                <div class="caption mb-2">MEVN Stack</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">June 2022</div>
+                <div class="caption font-weight-bold blue--text">June 2017</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Business Administration</strong>
+                <div class="caption mb-2">Anadolu University</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">June 2019</div>
+                <div class="caption font-weight-bold blue--text">June 2013</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Mechatronics Engineering </strong>
+                <div class="caption mb-2">Karabuk University</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-flex xs12 sm12 md4 lg6 xl6 class="mt-4 pt-2">
+      <h2 class="mb-4 pl-4">Experience</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">
+                  Dec 2020 -
+                </div>
+                <div class="caption font-weight-bold blue--text">Present</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Freelance Software Developer</strong>
+                <div class="caption mb-2">Remote</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">
+                  Jun 2017 -
+                </div>
+                <div class="caption font-weight-bold blue--text">Aug 2017</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Mechatronic Engineer - Intern</strong>
+                <div class="caption mb-2">Arcelik A.S</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="blue" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold blue--text">Jun 2016</div>
+                <div class="caption font-weight-bold blue--text">Aug 2016</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Mechatronic Engineer - Intern</strong>
+                <div class="caption mb-2">Hexagon Studio</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span class="blue--text">Skills</span>
+        </h2>
+        <br />
+        <strong>Vue</strong>
+        <v-progress-linear
+          v-model="buffer1"
+          :buffer-value="bufferValue"
+          color="blue"
+          height="16"
+        ></v-progress-linear>
+        <strong>Nuxt</strong>
+        <v-progress-linear
+          v-model="buffer2"
+          :buffer-value="bufferValue"
+          color="blue"
+          height="16"
+        ></v-progress-linear>
+        <strong>HTML/CSS</strong>
+        <v-progress-linear
+          v-model="buffer3"
+          :buffer-value="bufferValue"
+          color="blue"
+          height="16"
+        ></v-progress-linear>
+        <strong>JavaScript</strong>
+        <v-progress-linear
+          v-model="buffer4"
+          :buffer-value="bufferValue"
+          color="blue"
+          height="16"
+        ></v-progress-linear>
+      </v-flex>
+    </v-layout>
+  </v-layout>
+
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Resume',
+    titleTemplate: '%s ← Eralp Özcan',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'description',
+        content:
+          'Vue Nuxt HTML CSS JavaScript Coding Skills Resume Work Web Developer Front-End'
+      },
+      { charset: 'utf-8' },
+      { property: 'og:title', content: 'Eralp Özcan' },
+      { property: 'og:site_name', content: 'Eralp Özcan' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://eralpozcan.com' },
+      {
+        property: 'og:image',
+        content: 'https://i.imgur.com/Dcz2PGx.jp'
+      },
+      {
+        property: 'og:description',
+        content:
+          'Vue Nuxt HTML CSS JavaScript Coding Skills Resume Work Web Developer Front-End'
+      }
+    ]
+  },
+  data () {
+    return {
+      value: 0,
+      buffer1: 0,
+      buffer2: 0,
+      buffer3: 0,
+      buffer4: 0,
+      buffer5: 0,
+      buffer6: 0,
+      bufferValue: 100,
+      interval: 0
+    }
+  },
+  mounted () {
+    this.startBuffer()
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  },
+  methods: {
+    startBuffer () {
+      this.interval = setInterval(() => {
+        this.buffer1 = 75
+        this.buffer2 = 25
+        this.buffer3 = 60
+        this.buffer4 = 60
+        this.buffer5 = 90
+        this.buffer6 = 70
+      }, 800)
+    }
+  }
+}
+</script>
+<style></style>
