594f1f6e14f056072c854013fb60d3bec0ca625f
Eralpozcan <eralpozcans@gmail.com>
2022-06-02 18:58:25 +0300

    init

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const { routes } = require('./src/routes.js');
+
+
+module.exports = {
+    pluginOptions: {
+      prerenderSpa: {
+        registry: undefined,
+        renderRoutes: [
+          '/',
+          '/contact',
+          '/resume',
+          '/portfolio',
+          '/404',
+          '*',
+        ],
+        useRenderEvent: true,
+        onlyProduction: true,
+   
+        headless: false, // <- this could also be inside the customRendererConfig
+        customRendererConfig:
+        {
+          args: ["--auto-open-devtools-for-tabs"]
+        }
+      },
+      sitemap: {
+        baseURL: 'https://eralpozcan.com',
+        routes,
+        }
+    }
+  }
