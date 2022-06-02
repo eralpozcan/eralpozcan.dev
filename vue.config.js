583a3c0309f94fa86e7719056c80cb95fde6c827
Eralpozcan <eralpozcans@gmail.com>
2022-06-02 21:01:42 +0300

    Security test

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,6 @@
 
 module.exports = {
     pluginOptions: {
-      prerenderSpa: {
-        registry: undefined,
-        renderRoutes: [
-          '/',
-          '/contact',
-          '/resume',
-          '/portfolio',
-          '/404',
-        ],
-        useRenderEvent: true,
-        onlyProduction: true,
-        headless: false, // <- this could also be inside the customRendererConfig
-        customRendererConfig:
-        {
-          args: ["--auto-open-devtools-for-tabs"]
-        }
-      },
       sitemap: {
         baseURL: 'https://eralpozcan.com',
         }
