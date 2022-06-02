a8aeeb040464fb12c3280636cbf204d40517b352
Eralpozcan <eralpozcans@gmail.com>
2022-06-02 21:39:24 +0300

    Security test add prerenderSpa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 
 module.exports = {
     pluginOptions: {
-    },
+      prerenderSpa: {
+        registry: undefined,
+        renderRoutes: [
+          '/',
+          '/contact',
+          '/resume',
+          '/portfolio',
+          '/404',
+        ],
+        useRenderEvent: true,
+        onlyProduction: true,
+        headless: false, // <- this could also be inside the customRendererConfig
+        customRendererConfig:
+        {
+          args: ["--auto-open-devtools-for-tabs"]
+        }
+      },
+    }
   }
