9cd80a3df687ce0d21c6e1cc2aafcc6e6a04bd17
Eralpozcan <eralpozcans@gmail.com>
2023-11-10 01:15:40 +0300

    fix: card view link problem in project page

--- components/CardView.vue
+++ components/CardView.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="card lg:w-72 lg:h-72 xl:w-96 2xl:w-96 xl:h-96 2xl:h-96 bg-base-100 shadow-xl mx-8 my-4">
-    <figure><nuxt-img :src="data.openGraphImageUrl" :alt="data.name + 'preview image'" loading="lazy" width="400" height="190" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">
-        {{ data.name}}
-        <div v-if="compareDateWithCurrent(data.updatedAt)" class="badge badge-secondary" aria-label="New">NEW</div>
-      </h2>
-      <p>{{ data.description }}</p>
-      <div class="card-actions justify-center" >
-        <div class="badge badge-outline" :style="{color:usedLanguages.color}" v-for="usedLanguages in data.languages.nodes" :key="usedLanguages.name"> 
-          {{ usedLanguages.name }}</div> 
+    <nuxt-link :to="data.url" class="card-image-link">
+      <figure><nuxt-img :src="data.openGraphImageUrl" :alt="data.name + 'preview image'" loading="lazy" width="400" height="190" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">
+          {{ data.name}}
+          <div v-if="compareDateWithCurrent(data.updatedAt)" class="badge badge-secondary" aria-label="New">NEW</div>
+        </h2>
+        <p>{{ data.description }}</p>
+        <div class="card-actions justify-center" >
+          <div class="badge badge-outline" :style="{color:usedLanguages.color}" v-for="usedLanguages in data.languages.nodes" :key="usedLanguages.name"> 
+            {{ usedLanguages.name }}</div> 
+        </div>
       </div>
-    </div>
+    </nuxt-link>
   </div>
 </template>
 
