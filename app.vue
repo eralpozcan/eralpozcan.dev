e56342078c1469d91c61131d402feaf4b7be8e58
Eralpozcan <eralpozcans@gmail.com>
2024-08-12 22:05:29 +0300

    feat: add vercel speed-insights and remove ftp workflow

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <SiteHeader />
+    <NuxtLayout>
       <NuxtPage />
-    <SiteFooter />
+    </NuxtLayout>
   </div>
 </template>
 
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SiteHeader />
+      <slot />
+    <SiteFooter />
+  </div>
+  <SpeedInsights />
+</template>
+
+<script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
+<style>
+
+</style>
