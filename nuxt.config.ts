a3c8e3db2a71f8b7d5c9c0d062ce065f0b45cc43
eralpozcan <eralpozcans@gmail.com>
2024-10-22 22:09:51 +0300

    revert: closed sentry source map

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
     'nuxt-calendly',
     '@nuxtjs/seo',
     'nuxt-security',
-    '@sentry/nuxt/module',
+    // '@sentry/nuxt/module',
     '@nuxtjs/web-vitals'
   ],
 
